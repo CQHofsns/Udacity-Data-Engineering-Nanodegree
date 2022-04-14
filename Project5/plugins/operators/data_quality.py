151a9d33c2c17d19269652154d93546e35231572
QUANG HUY CHU <45648109+CQHofsns@users.noreply.github.com>
2022-04-15 02:30:24 +0900

    Final update

--- Project5/plugins/operators/data_quality.py
+++ Project5/plugins/operators/data_quality.py
@@ -12,7 +12,7 @@ class DataQualityOperator(BaseOperator):
                  # Example:
                  # conn_id = your-connection-name
                  redshift_conn_id= '',
-                 check_table_list= '',
+                 check_table_sql= '',
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
@@ -20,20 +20,25 @@ class DataQualityOperator(BaseOperator):
         # Example:
         # self.conn_id = conn_id
         self.redshift_conn_id= redshift_conn_id
-        self.check_table_list= check_table_list
+        self.check_table_sql= check_table_sql
 
     def execute(self, context):
         #self.log.info('DataQualityOperator not implemented yet')
         redshift_hook= PostgresHook(postgres_conn_id= self.redshift_conn_id)
         
-        for check_table in self.check_table_list:
-            print('Begin Data Quality evaluation process...')
-            records= redshift_hook.get_records(f'SELECT COUNT(*) FROM {check_table}')
-            if len(records) < 1 or len(records[0]) < 1:
-                print(f' ---> [ERROR]: Get no return records in {check_table} -->  Data Quality check failed.')
+        failed_test_cases= 0
+        
+        for sql in self.check_table_sql:
+            query= sql.get('check_sql')
+            expected_result= sql.get('expected_result')
             
-            number_records= records[0][0]
-            if number_records < 1:
-                print(f' ---> [ERROR]: {check_table} return 0 rows -->  Data Quality check failed.')
+            records = redshift.get_records(query)[0]
+            
+            if expected_result != records[0]:
+                print(f'Failed at query {query}')
+                failed_test_cases += 1
                 
-            print(f' ---> [INFO]: {check_table} pass the Data Quality check')
+        if failed_test_cases > 0:
+            print(f'{failed_test_cases} table test failed')
+            
+        print('All tables have passed the data quality check')
